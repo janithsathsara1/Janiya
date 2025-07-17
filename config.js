d8bd2e236b9afc5c59006b25d544d8c954a3f58e
janithsathsara1 <janithsathsara753@gmail.com>
2025-07-17 23:36:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-   alive_img:process.env.alive_img ||"IMG-20250717-WA0050.jpg".
+    alive_img:process.env.alive_img ||"IMG-20250717-WA0050.jpg",
+    alive_msg:orocess.env.alive_msg ||"Hello,I am yourname i am alive now",
 };
